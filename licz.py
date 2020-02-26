a35fef3274f61296927a055c6bca7983bc3efffb
MrDans-dev <56340448+MrDans-dev@users.noreply.github.com>
2020-02-26 10:52:10 +0100

    Update licz.py

---
+++ licz.py
@@ -0,0 +1,9 @@
+a = 1
+b = 2
+print(f"DZIALANIA NA A = {a} , B = {b}")
+print(f"Dodawanie {a+b}") #dodawanie 
+print(f"Odejmowanie {b-a}") #odejmowanie
+print(f"Mnozenie {a*b}") #mnozenie 
+print(f"Dzielenie {a/b}") #dzielenie
+print(f"Dzielenie bez reszty {a//b}") # dzielenie bez reszty
+print(f"Potega {b**a}") #potęga
