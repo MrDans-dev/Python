d0e2803d22ad9c52aee8f36beb0f159630b0171d
MrDans-dev <56340448+MrDans-dev@users.noreply.github.com>
2020-02-26 10:18:07 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 imie = input('Podaj Imie: ')
 uczelnia = input('Uczelnia: ')
 kierunek = input("Podaj kierunek: ")
-print("Witaj {a} z uczelni {b} z kierunku {c} ".format(a=imie , b=uczelnia , c=kierunek))
+wiek = int(input("Podaj wiek: "))
+print("Witaj {a} z uczelni {b} z kierunku {c} \nMasz {w} lat".format(a=imie , b=uczelnia , c=kierunek , w=wiek))
