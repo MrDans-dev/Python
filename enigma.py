5f61be0f72fb95cfd8c02e1e46fa62d863e08a1e
MrDans-dev <56340448+MrDans-dev@users.noreply.github.com>
2020-03-11 09:56:25 +0100

    Create

---
+++ enigma.py
@@ -0,0 +1,13 @@
+#x = 0
+#litera = []
+#for i in range(65, 91):
+#    litera[0] = chr(i)
+#    x += 1
+#    if i > 65 and x % 5 == 0:
+#        x = 0
+#    print(litera[i], end=" ")
+
+litera = []
+for i in range(65, 91):
+    litera.append(chr(i))
+print(litera[2])
