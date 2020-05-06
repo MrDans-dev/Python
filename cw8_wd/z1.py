be63bd5dacf3dfb2f35c39f107cab579be344506
MrDans-dev <56340448+MrDans-dev@users.noreply.github.com>
2020-05-06 11:44:48 +0200

    Update z1.py

--- cw8_wd/z1.py
+++ cw8_wd/z1.py
@@ -2,9 +2,6 @@ import numpy as np
 import pandas as pd
 import xlrd
 import openpyxl
-import os
-print("PLIKI: ",list(os.listdir()))
-print()
 xlsx = pd.ExcelFile('cw8_wd/imiona.xlsx')
 df = pd.read_excel(xlsx,'Imie')
 print(df)
