1f9d318814a90621de5e6ffa4fef42422ef016ef
MrDans-dev <56340448+MrDans-dev@users.noreply.github.com>
2020-05-21 22:12:55 +0200

    Update z2.py

--- cw8_wd/z2.py
+++ cw8_wd/z2.py
@@ -1,6 +1,14 @@
-import numpy as np
 import pandas as pd
 import xlrd
 xlsx = pd.ExcelFile('cw8_wd/imiona.xlsx')
 df = pd.read_excel(xlsx,'Imie')
-print(df)
+print(df[(df["Liczba"]>1000)])
+print()
+print(df[(df["Imie"] == "DANIEL")])
+print()
+print(df.groupby(['Rok']).agg({'Liczba':['sum']}))
+print()
+print(df[(df["Rok"]>=2000)&(df["Rok"]<=2005)].groupby("Rok").agg({'Liczba':['sum']}))
+print()
+print(df.groupby("Plec").agg({'Liczba':['sum']}))
+print()
