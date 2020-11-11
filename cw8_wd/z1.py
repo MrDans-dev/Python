5aafff955525f9687f39e48e3951636fcd9cf26b
MrDans-dev <56340448+MrDans-dev@users.noreply.github.com>
2020-11-11 22:32:31 +0100

    Update

--- cw8_wd/z1.py
+++ cw8_wd/z1.py
@@ -1,7 +1,5 @@
-import numpy as np
 import pandas as pd
 import xlrd
-import openpyxl
 xlsx = pd.ExcelFile('cw8_wd/imiona.xlsx')
 df = pd.read_excel(xlsx,'Imie')
 print(df)
--- cw9_wd/z2.py
+++ cw9_wd/z2.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import xlrd
 xlsx = pd.ExcelFile('cw9_wd/imiona.xlsx')
 df = pd.read_excel(xlsx,'Imie')
 df = df.groupby(['Plec']).agg({'Liczba':['sum']})
+print(df)
 wykres = df.plot.bar()
 plt.show()
