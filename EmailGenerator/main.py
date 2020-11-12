dc7d2c7b2df9d23368bf79e81d823643b739ccad
MrDans-dev <56340448+MrDans-dev@users.noreply.github.com>
2020-11-12 10:40:08 +0100

    Update main.py

--- EmailGenerator/main.py
+++ EmailGenerator/main.py
@@ -1,8 +1,33 @@
 import random
-e_adress = ['@onet.pl' , '@wp.pl' , '@gmail.com']
-name = input("Name: ")
-l_name = input("Last Name: ")
-
+e_adress = ['@onet.pl' , '@wp.pl' , '@gmail.com', '@poczta.onet.pl']
 e_adress_len = len(e_adress)
-while(1):
+
+def nick():
+    nick = input("Nick: ")
+    nick = str.lower(nick)
+    print(nick+str(random.randrange(100))+e_adress[random.randrange(e_adress_len)])
+
+
+
+
+def name():
+    name = input("Name: ")
+    l_name = input("Last Name: ")
+
+    name = str.lower(name)
+    l_name = str.lower(l_name)
     print(l_name+'.'+name+str(random.randrange(100))+e_adress[random.randrange(e_adress_len)])
+    print(name+str(random.randrange(100))+e_adress[random.randrange(e_adress_len)])
+    print(l_name+str(random.randrange(100))+e_adress[random.randrange(e_adress_len)])
+    print(l_name+name[0]+e_adress[random.randrange(e_adress_len)])
+
+
+print("1.Nick")
+print("2.Name Last Name")
+print("----------------")
+x = int(input())
+if x == 1:
+    nick()
+else:
+    name()
+
