27629a270c95ea8b087ee8887aa82dad546c32bf
MrDans-dev <56340448+MrDans-dev@users.noreply.github.com>
2020-11-12 12:51:26 +0100

    Update main.py

--- EmailGenerator/main.py
+++ EmailGenerator/main.py
@@ -6,6 +6,7 @@ def nick():
     nick = input("Nick: ")
     nick = str.lower(nick)
     print(nick+str(random.randrange(100))+e_adress[random.randrange(e_adress_len)])
+    print()
 
 
 
@@ -20,14 +21,33 @@ def name():
     print(name+str(random.randrange(100))+e_adress[random.randrange(e_adress_len)])
     print(l_name+str(random.randrange(100))+e_adress[random.randrange(e_adress_len)])
     print(l_name+name[0]+e_adress[random.randrange(e_adress_len)])
+    print(name+str(random.randrange(1900))+e_adress[random.randrange(e_adress_len)])
+    print()
+
+def comp():
+    name = input("Comp. Name: ")
+    name = str.lower(name)
+    name+='.pl'
+    a = ['biuro@' , 'kontakt@' , 'sekretariat@'];
+    for i in range(4):
+        print(a[random.randrange(len(a))]+name)
+    print()
 
 
-print("1.Nick")
-print("2.Name Last Name")
-print("----------------")
-x = int(input())
-if x == 1:
-    nick()
-else:
-    name()
+x=0
+while(x!=4):
+    print("1.Nick")
+    print("2.Name Last Name")
+    print("3.Company")
+    print("4.Exit")
+    print("----------------")
+    x = int(input("Option: "))
+    print()
+    if x == 1:
+        nick()
+    if x == 2:
+        name()
+    if x == 3:
+        comp()
+    
 
